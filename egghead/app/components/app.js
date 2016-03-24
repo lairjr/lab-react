70435bcbd09a8809d9fa96dfb3135a9bd371acfd
Lair Junior <ljunior@thoughtworks.com>
2016-03-24 19:51:21 -0300

    Finish egghead tutorial

--- egghead/app/components/app.js
+++ egghead/app/components/app.js
@@ -1,47 +1,28 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
+  constructor(){
     super();
     this.state = {
-      red: 0
-    };
-    this.update = this.update.bind(this);
+      data: [
+        {id: 1, name: 'Lair Júnior'},
+        {id: 2, name: 'Bruna Travi'}
+      ]
+    }
   }
-  update(e) {
-    this.setState(
-      {
-        red: ReactDom.findDOMNode(this.refs.red).value
-      }
-    );
-  }
-  render() {
+  render(){
+    let rows = this.state.data.map(p => {
+      return <PersonRow key={p.id} data={p} />
+    })
     return (
-      <div>
-        {this.state.red}
-        <NumInput ref="red" update={this.update} txt={this.state.txt} />
-      </div>
-    )
+      <table>
+        <tbody>{rows}</tbody>
+      </table>)
   }
 }
 
-class NumInput extends React.Component {
-  render() {
-    return (
-      <input type="range" min="0" max="255" onChange={this.props.update} />
-    )
-  }
+const PersonRow = (props) => {
+    return <tr><td>{props.data.id}</td><td>{props.data.name}</td></tr>
 }
 
-NumInput.propTypes = {
-    min: React.PropTypes.number,
-    max: React.PropTypes.number,
-    step: React.PropTypes.number,
-    val: React.PropTypes.number,
-    label: React.PropTypes.string,
-    update: React.PropTypes.func.isRequired,
-    type: React.PropTypes.oneOf(['number', 'range'])
-}
-
-export default App;
+export default App
